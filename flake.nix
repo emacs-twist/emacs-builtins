2e3d40f6fb073aee8284b83458fe423df0c6cffa
Akira Komamura <akira.komamura@gmail.com>
2023-10-15 12:51:30 +0900

    Put the Emacs versions provided from the actual executables

--- flake.nix
+++ flake.nix
@@ -49,9 +49,13 @@
             name = "${emacsName}.nix";
             path =
               pkgs.runCommand "build-${emacsName}-builtins-nix" {
+                buildInputs = [emacsPackage];
               } ''
+                EMACS_VERSION="$(emacs --version \
+                  | grep -F "GNU Emacs" \
+                  | grep -oE "[[:digit:]]+(:?\.[[:digit:]]+)+")"
                 echo >>$out '{'
-                echo >>$out '  version = "${emacsPackage.version}";'
+                echo >>$out "  version = \"''${EMACS_VERSION}\";"
                 echo >>$out '  libraries = ['
                 sed -e 's/^/"/' -e 's/$/"/' ${(pkgs.emacsTwist {
                     inherit emacsPackage;
